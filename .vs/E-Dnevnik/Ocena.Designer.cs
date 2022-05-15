cb5277cdeac35ebb302507eab88590b400966660
Onkiponki <pavle.georgijev@gmail.com>
2022-05-15 17:34:35 +0200

    .

---
+++ .vs/E-Dnevnik/Ocena.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace E_Dnevnik
+{
+    partial class Ocena
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbGodina = new System.Windows.Forms.ComboBox();
+            this.cmbProfesor = new System.Windows.Forms.ComboBox();
+            this.cmbOdeljenje = new System.Windows.Forms.ComboBox();
+            this.cmbPredmet = new System.Windows.Forms.ComboBox();
+            this.cmbUcenik = new System.Windows.Forms.ComboBox();
+            this.cmbOcena = new System.Windows.Forms.ComboBox();
+            this.datum = new System.Windows.Forms.DateTimePicker();
+            this.Delete = new System.Windows.Forms.Button();
+            this.update = new System.Windows.Forms.Button();
+            this.Insert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbGodina
+            // 
+            this.cmbGodina.FormattingEnabled = true;
+            this.cmbGodina.Location = new System.Drawing.Point(40, 66);
+            this.cmbGodina.Name = "cmbGodina";
+            this.cmbGodina.Size = new System.Drawing.Size(121, 21);
+            this.cmbGodina.TabIndex = 0;
+            // 
+            // cmbProfesor
+            // 
+            this.cmbProfesor.FormattingEnabled = true;
+            this.cmbProfesor.Location = new System.Drawing.Point(188, 66);
+            this.cmbProfesor.Name = "cmbProfesor";
+            this.cmbProfesor.Size = new System.Drawing.Size(121, 21);
+            this.cmbProfesor.TabIndex = 1;
+            // 
+            // cmbOdeljenje
+            // 
+            this.cmbOdeljenje.FormattingEnabled = true;
+            this.cmbOdeljenje.Location = new System.Drawing.Point(469, 66);
+            this.cmbOdeljenje.Name = "cmbOdeljenje";
+            this.cmbOdeljenje.Size = new System.Drawing.Size(121, 21);
+            this.cmbOdeljenje.TabIndex = 2;
+            // 
+            // cmbPredmet
+            // 
+            this.cmbPredmet.FormattingEnabled = true;
+            this.cmbPredmet.Location = new System.Drawing.Point(331, 66);
+            this.cmbPredmet.Name = "cmbPredmet";
+            this.cmbPredmet.Size = new System.Drawing.Size(121, 21);
+            this.cmbPredmet.TabIndex = 3;
+            // 
+            // cmbUcenik
+            // 
+            this.cmbUcenik.FormattingEnabled = true;
+            this.cmbUcenik.Location = new System.Drawing.Point(40, 128);
+            this.cmbUcenik.Name = "cmbUcenik";
+            this.cmbUcenik.Size = new System.Drawing.Size(121, 21);
+            this.cmbUcenik.TabIndex = 4;
+            // 
+            // cmbOcena
+            // 
+            this.cmbOcena.FormattingEnabled = true;
+            this.cmbOcena.Location = new System.Drawing.Point(188, 128);
+            this.cmbOcena.Name = "cmbOcena";
+            this.cmbOcena.Size = new System.Drawing.Size(121, 21);
+            this.cmbOcena.TabIndex = 5;
+            // 
+            // datum
+            // 
+            this.datum.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datum.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datum.Location = new System.Drawing.Point(452, 122);
+            this.datum.Name = "datum";
+            this.datum.Size = new System.Drawing.Size(163, 31);
+            this.datum.TabIndex = 6;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(642, 124);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(96, 27);
+            this.Delete.TabIndex = 25;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            // 
+            // update
+            // 
+            this.update.Location = new System.Drawing.Point(642, 76);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(96, 27);
+            this.update.TabIndex = 24;
+            this.update.Text = "Update";
+            this.update.UseVisualStyleBackColor = true;
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(642, 23);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(96, 27);
+            this.Insert.TabIndex = 23;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(326, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 25);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "ID";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 175);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(698, 271);
+            this.dataGridView1.TabIndex = 27;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 25);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Godina";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(183, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 25);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Profesor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(326, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 25);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Predmet";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(464, 25);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 25);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Odeljenje";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(35, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 25);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Ucenik";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(183, 94);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 25);
+            this.label7.TabIndex = 33;
+            this.label7.Text = "Ocena";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(464, 94);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 25);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "ID";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(326, 94);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(101, 25);
+            this.label9.TabIndex = 35;
+            this.label9.Text = "ID Ocene";
+            // 
+            // Ocena
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 481);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.datum);
+            this.Controls.Add(this.cmbOcena);
+            this.Controls.Add(this.cmbUcenik);
+            this.Controls.Add(this.cmbPredmet);
+            this.Controls.Add(this.cmbOdeljenje);
+            this.Controls.Add(this.cmbProfesor);
+            this.Controls.Add(this.cmbGodina);
+            this.Name = "Ocena";
+            this.Text = "Ocena";
+            this.Load += new System.EventHandler(this.Ocena_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbGodina;
+        private System.Windows.Forms.ComboBox cmbProfesor;
+        private System.Windows.Forms.ComboBox cmbOdeljenje;
+        private System.Windows.Forms.ComboBox cmbPredmet;
+        private System.Windows.Forms.ComboBox cmbUcenik;
+        private System.Windows.Forms.ComboBox cmbOcena;
+        private System.Windows.Forms.DateTimePicker datum;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ .vs/E-Dnevnik/Ocena.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace E_Dnevnik
+{
+    public partial class Ocena : Form
+    {
+        SqlConnection veza = Konekcija.zakonektuj();
+        string naredba;
+        public Ocena()
+        {
+            InitializeComponent();
+        }
+
+        private void Ocena_Load(object sender, EventArgs e)
+        {
+            FeelingTheCombosY();
+        }
+        private void FeelingTheCombosY()
+        {
+            naredba = "select * from Skolska_godina";
+            SqlDataAdapter adapter = new SqlDataAdapter(naredba, veza);
+            DataTable godina = new DataTable();
+            adapter.Fill(godina);
+            cmbGodina.DataSource = godina;
+            cmbGodina.ValueMember = "id";
+            cmbGodina.DisplayMember = "naziv";
+            cmbGodina.SelectedValue = 2;
+        }
+    }
+}
